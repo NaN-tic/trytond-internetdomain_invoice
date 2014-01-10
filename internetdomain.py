d0646ab9fbcab10ae42b9705ae805649c8ce012e
resteve <resteve@zikzakmedia.com>
2014-01-10 15:55:41 +0100

    Create invoice from object data

--- internetdomain.py
+++ internetdomain.py
@@ -62,91 +62,6 @@ class Renewal:
             ' / ' + str(self.date_expire) + ')')
         return description
 
-    @classmethod
-    def _get_invoice_renewal(self, renewal):
-        '''
-        Return invoice values for renewal
-        :param renewal: the BrowseRecord of the renewal
-        :return: a dict with renewal fields as key and renewal values as value
-        '''
-        Journal = Pool().get('account.journal')
-        PaymentTerm = Pool().get('account.invoice.payment_term')
-
-        journal_id = Journal.search([
-            ('type', '=', 'expense'),
-            ], limit=1)
-        if journal_id:
-            journal_id = journal_id[0]
-
-        payment_term_ids = PaymentTerm.search([('active', '=', True)])
-        if not len(payment_term_ids) > 0:
-            self.raise_user_error('missing_payment_term')
-
-        party = renewal.domain.party
-        if not party.account_receivable:
-            self.raise_user_error('missing_account_receivable',
-                error_args=(party.name, party))
-
-        invoice_address = party.address_get(type='invoice')
-
-        res = {
-            'company': renewal.domain.company.id,
-            'type': 'out_invoice',
-            'journal': journal_id,
-            'party': party.id,
-            'invoice_address': invoice_address and invoice_address or
-                renewal.domain.party_address,
-            'currency': renewal.domain.company.currency.id,
-            'account': party.account_receivable.id,
-            'payment_term': party.customer_payment_term and
-                party.customer_payment_term.id or payment_term_ids[0],
-            'description': renewal._get_invoice_description(),
-        }
-        return res
-
-    @classmethod
-    def _get_invoice_line_renewal(self, invoice, renewal, product, price=None):
-        '''
-        Return invoice line values for renewal
-        :param renewal: the BrowseRecord of the renewal
-        :param product: the BrowseRecord of the product
-        :param price: float
-        :return: a dict with renewal fields as key and renewal values as value
-        '''
-        InvoiceLine = Pool().get('account.invoice.line')
-
-        # Test if a revenue account exists for the product
-        product.account_revenue_used
-
-        line = InvoiceLine()
-        line.unit = 1
-        line.quantity = 1
-        line.product = product
-        line.invoice = invoice
-        line.description = None
-        line.party = renewal.domain.party
-        values = line.on_change_product()
-
-        res = {
-            'type': 'line',
-            'quantity': 1,
-            'unit': 1,
-            'product': product.id,
-            'product_uom_category': product.category and
-                product.category.id or None,
-            'account': values['account'],
-            'unit_price': values['unit_price'],
-            'taxes': [('add', product.customer_taxes_used)],
-            'description': '%s - %s' % (
-                    product.name,
-                    renewal._get_invoice_description(),
-                    ),
-            'sequence': 1,
-        }
-        if price:
-            res['unit_price'] = Decimal(price)
-        return res
-
 
 class CreateInvoice(ModelView):
     'Create Invoice'
@@ -175,19 +90,26 @@ class Invoice(Wizard):
 
         renewal = Renewal(Transaction().context['active_id'])
 
-        vals = Renewal._get_invoice_renewal(renewal)
-
-        with Transaction().set_user(0, set_context=True):
-            invoice = Invoice.create([vals])[0]
-
         product = self.ask.product
         price = self.ask.price
 
-        vals = Renewal._get_invoice_line_renewal(invoice, renewal, product, price)
-        vals['invoice'] = invoice.id
+        #Create Invoice
+        party = renewal.domain.party
+        description = renewal._get_invoice_description()
+        invoice_type = 'out_invoice'
+        invoice = Invoice.get_invoice_data(party, description, invoice_type)
         with Transaction().set_user(0, set_context=True):
-            InvoiceLine.create([vals])
+            invoice.save()
 
+        #Create Invoice line
+        quantity = 1
+        line = InvoiceLine.get_invoice_line_data(invoice, product, quantity)
+        if price:
+            line.unit_price = price
+        with Transaction().set_user(0, set_context=True):
+            line.save()
+
+        #Update taxes
         with Transaction().set_user(0, set_context=True):
             Invoice.update_taxes([invoice])
 
