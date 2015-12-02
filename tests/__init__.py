755990c5332355b6e246adb9d896881293d42cd3
resteve <resteve@zikzakmedia.com>
2015-12-02 09:45:25 +0100

    FIX loading Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-#This file is part account_invoice_cancel module for Tryton.
+#This file is part internetdomain_invoice module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 
-from .test_internetdomain import suite
+from .test_internetdomain_invoice import suite
 
 __all__ = ['suite']
